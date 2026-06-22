224aed537437056d1e3df270f67a52fd1ed2193e
royel21 <royelconsoro@hotmail.com>
2026-06-22 16:36:24 -0400

    more fix

--- #registry_3LDK_DB.bat
+++ #registry_3LDK_DB.bat
@@ -6,30 +6,30 @@ rem Get the current folder path
 set "folder=%~dp0"
 if "%folder:~-1%"=="\" set "folder=%folder:~0,-1%"
 
-set "CompanyKey=HKLM\SOFTWARE\Wow6432Node\3LDK_DB"
+set "%CompanyKey%=HKLM\SOFTWARE\Wow6432Node\3LDK_DB"
 
 rem Add the "SetupType" string value
-reg add "CompanyKey" /v "SetupType" /t REG_SZ /d "%folder%" /f
+reg add "%CompanyKey%" /v "SetupType" /t REG_SZ /d "%folder%" /f
 
 rem Add the "SetupFolder" string value
-reg add "CompanyKey" /v "SetupFolder" /t REG_SZ /d "C:\\" /f 
+reg add "%CompanyKey%" /v "SetupFolder" /t REG_SZ /d "C:\\" /f 
 
 rem Add the "SrcFolder" string value
-reg add "CompanyKey" /v "SrcFolder" /t REG_SZ /d "%folder%" /f
+reg add "%CompanyKey%" /v "SrcFolder" /t REG_SZ /d "%folder%" /f
 
 rem Add the "Graphic" string value
-reg add "CompanyKey" /v "Graphic" /t REG_SZ /d "%folder%" /f 
+reg add "%CompanyKey%" /v "Graphic" /t REG_SZ /d "%folder%" /f 
 
 rem Add the "Script" string value
-reg add "CompanyKey" /v "Script" /t REG_SZ /d "%folder%" /f 
+reg add "%CompanyKey%" /v "Script" /t REG_SZ /d "%folder%" /f 
 
 rem Add the "Sound" string value
-reg add "CompanyKey" /v "Sound" /t REG_SZ /d "%folder%" /f 
+reg add "%CompanyKey%" /v "Sound" /t REG_SZ /d "%folder%" /f 
 
 rem Add the "Voice" string value
-reg add "CompanyKey" /v "Voice" /t REG_SZ /d "%folder%" /f 
+reg add "%CompanyKey%" /v "Voice" /t REG_SZ /d "%folder%" /f 
 
 rem Add the "Voice2" string value
-reg add "CompanyKey" /v "Voice2" /t REG_SZ /d "%folder%" /f 
+reg add "%CompanyKey%" /v "Voice2" /t REG_SZ /d "%folder%" /f 
 
 endlocal
